4642a1c4c647ceff82797116d940d06d4e61f59f
William Souza <willmiranda@gmail.com>
2015-07-13 16:58:41 -0300

    Bug fix QueryParams is not a class

--- lib/query_params.rb
+++ lib/query_params.rb
@@ -2,34 +2,36 @@ require "query_params/version"
 require "query_params/filter"
 require "query_params/condition"
 
-class QueryParams
+module URI
+  class QueryParams
 
-  include Filter
+    include Filter
 
-  def initialize(base_uri = "")
-    @base_uri = base_uri.nil? ? "" : base_uri
-    @params = []
-    @query = ""
-  end
-
-  def build_uri()
-    uri = ""
-    unless @base_uri.empty?
-      uri += uri.include?('?') ? "&" : "?"
+    def initialize(base_uri = "")
+      @base_uri = base_uri.nil? ? "" : base_uri
+      @params = []
+      @query = ""
     end
-    unless @query.empty?
-      uri += "#{@query}" 
-      uri += "&" unless @params.empty?
-    end
-    uri += "filters=#{ERB::Util.url_encode(@params.join("|"))}" unless @params.empty?
-    uri = "" if uri.length == 1
-    "#{@base_uri}#{uri}"
-  end
 
-  def self.build_uri(options = {})
-    queryParams = QueryParams.new(options[:base_uri])
-    Condition.build_uri(queryParams, options[:conditions]) if options[:conditions]
-    queryParams.full_text_search(options[:q]) if options[:q]
-    queryParams.build_uri()
+    def build_uri()
+      uri = ""
+      unless @base_uri.empty?
+        uri += uri.include?('?') ? "&" : "?"
+      end
+      unless @query.empty?
+        uri += "#{@query}" 
+        uri += "&" unless @params.empty?
+      end
+      uri += "filters=#{ERB::Util.url_encode(@params.join("|"))}" unless @params.empty?
+      uri = "" if uri.length == 1
+      "#{@base_uri}#{uri}"
+    end
+
+    def self.build_uri(options = {})
+      queryParams = QueryParams.new(options[:base_uri])
+      Condition.build_uri(queryParams, options[:conditions]) if options[:conditions]
+      queryParams.full_text_search(options[:q]) if options[:q]
+      queryParams.build_uri()
+    end
   end
 end
