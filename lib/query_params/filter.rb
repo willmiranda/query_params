ecad0ca8271b9d7d10e6eeafc9ef376b1d13e2b6
William Souza <willmiranda@gmail.com>
2015-07-16 14:13:55 -0300

    Checking if value is empty before assigning a filter

--- lib/query_params/filter.rb
+++ lib/query_params/filter.rb
@@ -1,7 +1,7 @@
 module Filter
   
   def full_text_search(query)
-    @query = "q=#{query.strip}"
+    @query = "q=#{query.strip}" unless query.to_s.empty?
   end
 
   def q(query)
@@ -10,9 +10,9 @@ module Filter
 
   def equal(key, value)
     if value.kind_of?(Array)
-      @params.push("#{key}::in(#{value.join(",")})")
+      @params.push("#{key}::in(#{value.join(",")})") unless value.to_s.empty?
     else
-      @params.push("#{key}::eq(#{value})")
+      @params.push("#{key}::eq(#{value})") unless value.to_s.empty?
     end
   end
 
@@ -21,11 +21,11 @@ module Filter
   end
 
   def in(key, values)
-    @params.push("#{key}::in(#{values.join(",")})")
+    @params.push("#{key}::in(#{values.join(",")})") unless values.to_s.empty?
   end
 
   def greater_and_equal(key, value)
-    @params.push("#{key}::ge(#{value})")
+    @params.push("#{key}::ge(#{value})") unless value.to_s.empty?
   end
     
   def ge(key, value)
@@ -33,7 +33,7 @@ module Filter
   end
 
   def less_and_equal(key, value)
-    @params.push("#{key}::le(#{value})")
+    @params.push("#{key}::le(#{value})") unless value.to_s.empty?
   end
 
   def le(key, value)
@@ -41,7 +41,7 @@ module Filter
   end
 
   def between(key, range_ini, range_end)
-    @params.push("#{key}::bt(#{range_ini},#{range_end})")
+    @params.push("#{key}::bt(#{range_ini},#{range_end})") if !range_ini.to_s.empty? && !range_end.to_s.empty?
   end
 
   def bt(key, range_ini, range_end)
--- lib/query_params/version.rb
+++ lib/query_params/version.rb
@@ -1,3 +1,3 @@
 module QueryParams
-  VERSION = "1.4.4"
+  VERSION = "1.4.5"
 end
